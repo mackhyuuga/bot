bfc20a5698e752034db29bb71074e060145e0b0e
allison eduardo <mackhyuuga@hotmail.com>
2020-12-30 15:00:14 -0300

    .

--- bot_python.py
+++ bot_python.py
@@ -3,17 +3,21 @@ from time import sleep
 
 class TinderBot():
     def __init__(self):
-        self.driver = webdriver.Chrome(executable_path=r'./chromedriver')
+        self.driver = webdriver.Chrome(executable_path=r'/home/mack/Documents/python/bot/chromedriver')
 
     def login(self):
         self.driver.get('https://tinder.com/?lang=pt')
 
-        sleep(60)
+        sleep(40)
         
         for i in range(1, 10000):
             sleep(0.5)
-            like = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
-            like.click()
+            try:
+                like = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+                like.click()
+            except Exception as error:
+                print("the following error occurred: ", error)
+                sleep(30)
     
 bot = TinderBot()
 bot.login()
